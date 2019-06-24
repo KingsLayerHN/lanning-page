fb216e521a47fc4a595fd4e438e9f5d4c8258980
Kinglayer <lesterarte@gmail.com>
2019-06-23 19:03:40 -0600

    agregando dependencias en lanning page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'popper.js';
+window.jQuery = window.$ = $;
+require ('datatables');
 
 ReactDOM.render(
   <App />,
